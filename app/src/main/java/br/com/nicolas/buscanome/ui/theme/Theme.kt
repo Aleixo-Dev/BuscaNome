55073c2fbdf3b4dac499f71935d892244a7a8cee
Aleixo-Dev <nicolasaleixo2020@gmail.com>
2023-01-20 14:05:52 -0300

    remove dark theme

--- app/src/main/java/br/com/nicolas/buscanome/ui/theme/Theme.kt
+++ app/src/main/java/br/com/nicolas/buscanome/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package br.com.nicolas.buscanome.ui.theme
 
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
@@ -17,23 +16,11 @@ private val LightColorPalette = lightColors(
     primaryVariant = Purple700,
     secondary = Teal200
 
-    /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
 )
 
 @Composable
-fun BuscaNomeTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
-    val colors = if (darkTheme) {
-        DarkColorPalette
-    } else {
-        LightColorPalette
-    }
+fun BuscaNomeTheme(content: @Composable () -> Unit) {
+    val colors = LightColorPalette
 
     MaterialTheme(
         colors = colors,
